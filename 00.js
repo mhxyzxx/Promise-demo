fbdaa12f1624d012764c0e0c59fc5d37f9152d72
mhxyzxx <mhxyzxx@163.com>
2019-11-06 23:52:37 +0800

    把文件延迟2秒执行，然后汇总到e文件中

--- 00.js
+++ 00.js
@@ -1,27 +1,35 @@
 const fs = require('fs');
-fs.readFile('./data/a.txt', 'utf8', (err, retA) => {
-    if (err) {
-        throw err
-    }
-    console.log(retA);
-    fs.readFile('./data/b.txt', 'utf8', (err, retB) => {
+setTimeout(function() {
+    fs.readFile('./data/a.txt', 'utf8', (err, retA) => {
         if (err) {
             throw err
         }
-        console.log(retB);
-        fs.readFile('./data/c.txt', 'utf8', (err, retC) => {
-            if (err) {
-                throw err
-            }
-            console.log(retC);
-            // 把读到的三个文件汇总到一个d.txt文件中
-            const data = retA + retB + retC;
-            fs.writeFile('./data/d.txt', data, err => {
+        console.log(retA);
+        setTimeout(function() {
+            fs.readFile('./data/b.txt', 'utf8', (err, retB) => {
                 if (err) {
                     throw err
                 }
-                console.log('success');
+                console.log(retB);
+                setTimeout(function() {
+                    fs.readFile('./data/c.txt', 'utf8', (err, retC) => {
+                        if (err) {
+                            throw err
+                        }
+                        console.log(retC);
+                        // 把读到的三个文件汇总到一个d.txt文件中
+                        const data = retA + retB + retC;
+                        setTimeout(function() {
+                            fs.writeFile('./data/e.txt', data, err => {
+                                if (err) {
+                                    throw err
+                                }
+                                console.log('success');
+                            })
+                        }, 4000)
+                    })
+                }, 3000)
             })
-        })
+        }, 2000)
     })
-})
+}, 1000)
