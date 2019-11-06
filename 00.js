7d63f74d492ddff4024afd2c711d2a1c5f798cd5
mhxyzxx <mhxyzxx@163.com>
2019-11-06 23:39:34 +0800

    使用writeFile把a,b,c文件汇总到d文件中

--- 00.js
+++ 00.js
@@ -1,19 +1,27 @@
 const fs = require('fs');
-fs.readFile('./data/a.txt', 'utf8', (err, ret) => {
+fs.readFile('./data/a.txt', 'utf8', (err, retA) => {
     if (err) {
         throw err
     }
-    console.log(ret);
-    fs.readFile('./data/b.txt', 'utf8', (err, ret) => {
+    console.log(retA);
+    fs.readFile('./data/b.txt', 'utf8', (err, retB) => {
         if (err) {
             throw err
         }
-        console.log(ret);
-        fs.readFile('./data/c.txt', 'utf8', (err, ret) => {
+        console.log(retB);
+        fs.readFile('./data/c.txt', 'utf8', (err, retC) => {
             if (err) {
                 throw err
             }
-            console.log(ret);
+            console.log(retC);
+            // 把读到的三个文件汇总到一个d.txt文件中
+            const data = retA + retB + retC;
+            fs.writeFile('./data/d.txt', data, err => {
+                if (err) {
+                    throw err
+                }
+                console.log('success');
+            })
         })
     })
 })
