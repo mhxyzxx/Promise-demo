5c0c398576f2a854571db219183e21a32ebd5272
mhxyzxx <mhxyzxx@163.com>
2019-11-09 13:15:43 +0800

    封装Promise 版的readFile方法

--- 04-readFile.js
+++ 04-readFile.js
@@ -15,7 +15,44 @@ function readFile(filePath) { // 根据需要定义参数
     return ppp
 }
 
-readFile('./data/a.txt')
+// 测试一个使用Promise封装的一个读文件的方法
+// readFile('./data/a.txt')
+//     .then(function(data) {
+//         console.log(data.toString()); // hello aaa
+//     })
+
+// 封装的一个Promise版的定时器
+function sleep(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            resolve()
+        }, time)
+    })
+}
+
+// 需求：等1秒读a文件，等2秒读b文件，等3秒读c文件
+sleep(1000)
+    .then(function() {
+        console.log('1 秒到了')
+        return readFile('./data/a.txt')
+    })
     .then(function(data) {
-        console.log(data.toString()); // hello aaa
+        console.log('a文件读取到了 => ', data.toString())
+        return sleep(2000)
+    })
+    .then(function() {
+        console.log('2 秒到了')
+        return readFile('./data/b.txt')
+    })
+    .then(function(data) {
+        console.log('b文件读取到了 => ', data.toString())
+        return sleep(3000)
+    })
+    .then(function() {
+        console.log('3 秒到了')
+        return readFile('./data/c.txt')
+    })
+    .then(function(data) {
+        console.log(data.toString())
+        console.log('The end.')
     })
